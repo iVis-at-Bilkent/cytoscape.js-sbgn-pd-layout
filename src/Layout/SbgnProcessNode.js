ddb0ce837ca4b90411fd7a52d66a475b183d0acc
alperkaracelik <alper.karacelik@gmail.com>
2016-12-02 10:26:44 +0300

    Convertion of SbgnProcessNode and VisibilityEdge classes is completed.

--- src/Layout/SbgnProcessNode.js
+++ src/Layout/SbgnProcessNode.js
@@ -2,6 +2,7 @@ var IGeometry = require('./IGeometry');
 var PointD = require('./PointD');
 
 var SbgnPDNode = require('./SbgnPDNode');
+var SbgnPDEdge = require('./SbgnPDEdge');
 var SbgnPDConstants = require('./SbgnPDConstants');
 
 SbgnProcessNode.prototype.OrientationEnum = 
@@ -580,6 +581,429 @@ SbgnProcessNode.prototype.moveOneEdgeNodes = function (oneEdgeNodes, multiEdgeNo
 
         s.setCenter(newPoint.x, newPoint.y);
     }
+};
+
+/**
+* Identify single-edge effectors. Note that a process may have a number of
+* effectors. Find the location of each effector. If the orientation of
+* process node is vertical, ideal position of effectors is on the
+* horizontal directions. (or vice versa)
+*/
+SbgnProcessNode.prototype.approximateEffectors = function ()
+{
+    var newPoint = new PointD();
+    var approximationPnt = new PointD();
+
+    // identify the effectors
+    var numOfEffectorEdges = this.effectorEdges.length;
+    for (var i=0; i<numOfEffectorEdges; i++)
+    {
+        var edge = this.effectorEdges[i];
+        
+        // source node of each effector edge is the effector itself. only
+        // move single effectors
+        if (edge.getSource().getEdges().length !== 1)
+        {
+            continue;
+        }
+
+        approximationPnt = this.findEffectorTargetPoint(edge.getSource());
+
+        // place effector in a circular area using some randomness
+        newPoint.x = approximationPnt.x
+                        + (Math.random() * SbgnPDConstants.APPROXIMATION_DISTANCE * 2)
+                        - SbgnPDConstants.APPROXIMATION_DISTANCE;
+        newPoint.y = approximationPnt.y
+                        + (Math.random() * SbgnPDConstants.APPROXIMATION_DISTANCE * 2)
+                        - SbgnPDConstants.APPROXIMATION_DISTANCE;
+
+        edge.getSource().setCenter(newPoint.x, newPoint.y);
+    }
+};
+
+/**
+* Given the effector and its corresponding process, the method returns the
+* ideal position of the effector node, which has a distance of ideal edge
+* length from its process.
+*/
+SbgnProcessNode.prototype.findEffectorTargetPoint = function (eff)
+{
+    var approximationPnt = new PointD();
+    
+    // find target point
+    if (this.isHorizontal())
+    {
+        approximationPnt.x = this.getCenterX();
+
+        if (eff.getCenterY() > this.getCenterY())
+        {
+            approximationPnt.y = this.getCenterY() + this.idealEdgeLength;
+        }
+        else
+        {
+            approximationPnt.y = this.getCenterY() - this.idealEdgeLength;
+        }
+    }
+    else if (this.isVertical())
+    {
+        approximationPnt.y = this.getCenterY();
+
+        if (eff.getCenterX() > this.getCenterX())
+        {
+            approximationPnt.x = this.getCenterX() + this.idealEdgeLength;
+        }
+        else
+        {
+            approximationPnt.x = this.getCenterX() - this.idealEdgeLength;
+        }
+    }
+
+    return approximationPnt;
+};
+
+SbgnProcessNode.prototype.findPortTargetPoint = function (isInputPort, orient)
+{
+    if (orient === this.OrientationEnum.LEFT_TO_RIGHT)
+    {
+        if (isInputPort)
+        {
+            return new PointD((this.inputPort.getCenterX() - this.idealEdgeLength),
+                              this.inputPort.getCenterY());
+        }
+        else
+        {
+            return new PointD((this.outputPort.getCenterX() + this.idealEdgeLength),
+                              this.outputPort.getCenterY());
+        }
+    }
+    else if (orient === this.OrientationEnum.RIGHT_TO_LEFT)
+    {
+        if (isInputPort)
+        {
+            return new PointD((this.inputPort.getCenterX() + this.idealEdgeLength),
+                              this.inputPort.getCenterY());
+        }
+        else
+        {
+            return new PointD((this.outputPort.getCenterX() - this.idealEdgeLength),
+                              this.outputPort.getCenterY());
+        }
+    }
+    else if (orient === this.OrientationEnum.TOP_TO_BOTTOM)
+    {
+        if (isInputPort)
+        {
+            return new PointD(this.inputPort.getCenterX(),
+                              (this.inputPort.getCenterY() - this.idealEdgeLength));
+        }
+        else
+        {
+            return new PointD(this.outputPort.getCenterX(),
+                              (this.outputPort.getCenterY() + this.idealEdgeLength));
+        }
+    }
+    else if (orient === this.OrientationEnum.BOTTOM_TO_TOP)
+    {
+        if (isInputPort)
+        {
+            return new PointD(this.inputPort.getCenterX(),
+                              (this.inputPort.getCenterY() + this.idealEdgeLength));
+        }
+        else
+        {
+            return new PointD(this.outputPort.getCenterX(),
+                              (this.outputPort.getCenterY() - this.idealEdgeLength));
+        }
+    }
+
+    return null;
+};
+
+SbgnProcessNode.prototype.initLists = function ()
+{
+    var numOfConsumptionEdges = this.inputPort.getEdges().length;
+    for (var i=0; i<numOfConsumptionEdges; i++)
+    {
+        var edge = this.inputPort.getEdges()[i];
+        
+        if (!edge.isRigidEdge())
+        {
+            this.consumptionEdges.add(edge);
+        }
+    }
+
+    var numOfProductEdges = this.outputPort.getEdges().length;
+    for (var i=0; i<numOfProductEdges; i++)
+    {
+        var edge = this.outputPort.getEdges()[i];
+
+        if (!edge.isRigidEdge())
+        {
+            this.productEdges.add(edge);
+        }
+    }
+
+    // detect all effector nodes connected to this process node (if
+    // there are any)
+    var numOfEffectorEdges = this.parentCompound.getEdges().length;
+    for (var i=0; i<numOfEffectorEdges; i++)
+    {
+        var edge = this.parentCompound.getEdges()[i];
+
+        if (edge.isEffector())
+        {
+            this.effectorEdges.add(edge);
+        }
+    }
+};
+
+SbgnProcessNode.prototype.saveInformation = function (isInputPort, nodeIndex, angle)
+{
+
+    // remember angles especially for debug purposes
+    if (isInputPort)
+    {
+        this.consumptionEdges[nodeIndex].correspondingAngle = angle;
+    }
+    else
+    {
+        this.productEdges[nodeIndex].correspondingAngle = angle;
+    }
+
+    // note if the edges are properly oriented
+    if (Math.abs(angle) <= SbgnPDConstants.ANGLE_TOLERANCE)
+    {
+        if (isInputPort)
+        {
+            this.consumptionEdges[nodeIndex].isProperlyOriented = true;
+        }
+        else
+        {
+            this.productEdges[nodeIndex].isProperlyOriented = true;
+        }
+    }
+    else
+    {
+        if (isInputPort)
+        {
+            this.consumptionEdges[nodeIndex].isProperlyOriented = false;
+        }
+        else
+        {
+            this.productEdges[nodeIndex].isProperlyOriented = false;
+        }
+    }
+};
+
+SbgnProcessNode.prototype.isLeft = function (a, b, c, type)
+{
+    if (((b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)) > 0)
+    {
+        // left turn
+        if (this.orientation === this.OrientationEnum.TOP_TO_BOTTOM)
+        {
+            if (type === SbgnPDConstants.INPUT_PORT)
+                return -1;
+            else if (type === SbgnPDConstants.OUTPUT_PORT)
+                return 1;
+        }
+        else if (this.orientation === this.OrientationEnum.BOTTOM_TO_TOP)
+        {
+            if (type === SbgnPDConstants.INPUT_PORT)
+                return 1;
+            else if (type === SbgnPDConstants.OUTPUT_PORT)
+                return -1;
+        }
+        else if (this.orientation === this.OrientationEnum.LEFT_TO_RIGHT)
+        {
+            if (type === SbgnPDConstants.INPUT_PORT)
+                return 1;
+            else if (type === SbgnPDConstants.OUTPUT_PORT)
+                return -1;
+        }
+        else if (this.orientation === this.OrientationEnum.RIGHT_TO_LEFT)
+        {
+            if (type === SbgnPDConstants.INPUT_PORT)
+                return -1;
+            else if (type === SbgnPDConstants.OUTPUT_PORT)
+                return 1;
+        }
+    }
+    else
+    {
+        // right turn
+        if (this.orientation === this.OrientationEnum.TOP_TO_BOTTOM)
+        {
+            if (type === SbgnPDConstants.INPUT_PORT)
+                return 1;
+            else if (type === SbgnPDConstants.OUTPUT_PORT)
+                return -1;
+        }
+        else if (this.orientation === this.OrientationEnum.BOTTOM_TO_TOP)
+        {
+            if (type === SbgnPDConstants.INPUT_PORT)
+                return -1;
+            else if (type === SbgnPDConstants.OUTPUT_PORT)
+                return 1;
+        }
+        else if (this.orientation === this.OrientationEnum.LEFT_TO_RIGHT)
+        {
+            if (type === SbgnPDConstants.INPUT_PORT)
+                return -1;
+            else if (type === SbgnPDConstants.OUTPUT_PORT)
+                return 1;
+        }
+        else if (this.orientation === this.OrientationEnum.RIGHT_TO_LEFT)
+        {
+            if (type === SbgnPDConstants.INPUT_PORT)
+                return 1;
+            else if (type === SbgnPDConstants.OUTPUT_PORT)
+                return -1;
+        }
+    }
+    return 0;
+};
+
+SbgnProcessNode.prototype.copyNode = function (s, graphManager) // TODO: graphManager is not used.
+{
+    this.type = s.type;
+    this.label = s.label;
+    this.parentCompound = s.parentCompound;
+    this.inputPort = s.inputPort;
+    this.outputPort = s.outputPort;
+    this.setCenter(s.getCenterX(), s.getCenterY());
+    this.setChild(s.getChild());
+    this.setHeight(s.getHeight());
+    this.setLocation(s.getLocation().x, s.getLocation().y);
+    this.setNext(s.getNext());
+    this.setOwner(s.getOwner());
+    this.setPred1(s.getPred1());
+    this.setPred2(s.getPred2());
+    this.setWidth(s.getWidth());
+};
+
+SbgnProcessNode.prototype.copyFromSBGNPDNode = function (s, graphManager)
+{
+    this.type = s.type;
+    this.label = s.label;
+    this.setCenter(s.getCenterX(), s.getCenterY());
+    this.setChild(s.getChild());
+    this.setHeight(s.getHeight());
+    this.setLocation(s.getLocation().x, s.getLocation().y);
+    this.setNext(s.getNext());
+    this.setOwner(s.getOwner());
+    this.setPred1(s.getPred1());
+    this.setPred2(s.getPred2());
+    this.setWidth(s.getWidth());
+
+    // copy edges
+    var numOfEdges = s.getEdges().length;
+    for (var i=0; i<numOfEdges; i++)
+    {
+        var edge = s.getEdges()[i];
+        var newEdge = new SbgnPDEdge(edge.getSource(), edge.getTarget(), null, edge.type);
+
+        newEdge.copy(edge);
+
+        if (edge.getSource() === s)
+        {
+            newEdge.setSource(this);
+        }
+        else if (edge.getTarget() === s)
+        {
+            newEdge.setTarget(this);
+        }
+
+        // add new edge to the graph manager.
+        graphManager.add(newEdge, newEdge.getSource(), newEdge.getTarget());
+    }
+};
+
+SbgnProcessNode.prototype.isVertical = function ()
+{
+    if (this.orientation === this.OrientationEnum.TOP_TO_BOTTOM || 
+        this.orientation === this.OrientationEnum.BOTTOM_TO_TOP)
+    {
+        return true;
+    }
+    else
+    {
+        return false;   
+    }
+    
+};
+
+SbgnProcessNode.prototype.isHorizontal = function ()
+{
+    if (this.orientation === this.OrientationEnum.LEFT_TO_RIGHT || 
+        this.orientation === this.OrientationEnum.RIGHT_TO_LEFT)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
+
+SbgnProcessNode.prototype.setOrientation = function (orient)
+{
+    this.orientation = orient;
+    if (this.orientation === this.OrientationEnum.LEFT_TO_RIGHT)
+    {
+        this.inputPort.setCenter(this.getCenterX()
+                        - SbgnPDConstants.RIGID_EDGE_LENGTH, this.getCenterY());
+        this.outputPort.setCenter(this.getCenterX()
+                        + SbgnPDConstants.RIGID_EDGE_LENGTH, this.getCenterY());
+    }
+    else if (this.orientation === this.OrientationEnum.RIGHT_TO_LEFT)
+    {
+        this.inputPort.setCenter(this.getCenterX()
+                        + SbgnPDConstants.RIGID_EDGE_LENGTH, this.getCenterY());
+        this.outputPort.setCenter(this.getCenterX()
+                        - SbgnPDConstants.RIGID_EDGE_LENGTH, this.getCenterY());
+    }
+    else if (this.orientation === this.OrientationEnum.BOTTOM_TO_TOP)
+    {
+        this.inputPort.setCenter(this.getCenterX(), this.getCenterY()
+                        + SbgnPDConstants.RIGID_EDGE_LENGTH);
+        this.outputPort.setCenter(this.getCenterX(), this.getCenterY()
+                        - SbgnPDConstants.RIGID_EDGE_LENGTH);
+    }
+    else if (this.orientation === this.OrientationEnum.TOP_TO_BOTTOM)
+    {
+        this.inputPort.setCenter(this.getCenterX(), this.getCenterY()
+                        - SbgnPDConstants.RIGID_EDGE_LENGTH);
+        this.outputPort.setCenter(this.getCenterX(), this.getCenterY()
+                        + SbgnPDConstants.RIGID_EDGE_LENGTH);
+    }
+};
+
+/**
+* Transfer forces acting on process node to its parent compound to make
+* sure process does not move.
+*/
+SbgnProcessNode.prototype.transferForces = function ()
+{
+    this.parentCompound.springForceX += this.springForceX
+                    + this.inputPort.springForceX + this.outputPort.springForceX;
+    this.parentCompound.springForceY += this.springForceY
+                    + this.inputPort.springForceY + this.outputPort.springForceY;
+};
+
+SbgnProcessNode.prototype.getInputPort = function ()
+{
+    return this.inputPort;
+};
+
+SbgnProcessNode.prototype.getOutputPort = function ()
+{
+    return this.outputPort;
+};
+
+SbgnProcessNode.prototype.getParentCompound = function ()
+{
+    return parentCompound;
 };
 
 module.exports = SbgnProcessNode;
--- src/Layout/VisibilityEdge.js
+++ src/Layout/VisibilityEdge.js
@@ -9,4 +9,28 @@ for (var prop in CoSEEdge) {
   VisibilityEdge[prop] = CoSEEdge[prop];
 }
 
+/**
+* We want the length of a visibility edge calculated as the distance
+* between borders of two nodes, not the distance between center points.
+* Edges have to be vertical or horizontal.
+*/
+// @Override
+VisibilityEdge.prototype.updateLength = function ()
+{
+    if (this.source.getBottom() <= this.target.getTop())
+    {
+        this.lengthX = 0;
+        this.lengthY = this.source.getBottom() - this.target.getTop();
+    } 
+    else if (this.source.getRight() <= this.target.getLeft())
+    {
+        this.lengthX = this.source.getRight() - this.target.getLeft();
+        this.lengthY = 0;
+    }
+    // else
+    // System.out.println("unexpected edge");
+
+    this.length = Math.sqrt((this.lengthX * this.lengthX) + (this.lengthY * this.lengthY));
+};
+
 module.exports = VisibilityEdge;
